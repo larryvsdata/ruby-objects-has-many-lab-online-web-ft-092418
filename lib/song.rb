80dde160138989ec5ad75cecf6eb29953e7b3a8c
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-04 16:11:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,14 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name , :artist
  
   def initialize(name)
     @name = name
-    @songs = []
+    
   end
  
   def add_song(song)
     @songs << song
-    song.artist = self
+    
   end
  
   def songs
