1850ab1f9a0088c92cbfb72cd4a6fccbbf2c50f0
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-04 16:12:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,20 +6,10 @@ class Song
     
   end
  
-  def add_song(song)
-    @songs << song
-    
-  end
+
  
-  def songs
-    @songs
-  end
+
   
-  def add_song_by_name(name, genre)
-    song = Song.new(name, genre)
-    @songs << song
-    song.artist = self
-  end
-  
+
   
 end
